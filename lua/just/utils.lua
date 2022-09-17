80ab6315fb50c8899d83ee99b0075d695c590a05
Beat Scherrer <b.scherrer@mt-robot.com>
2022-09-17 15:27:26 +0200

    document utils

--- lua/just/utils.lua
+++ lua/just/utils.lua
@@ -1,13 +1,20 @@
 local M = {}
 
+---comment Helper to print a table.
+---@param argument table Table to print.
 function M.printTable(argument)
 	print(vim.inspect(argument))
 end
 
+---comment Reloads the plugin.
 function M.reloadPlugin()
 	require("plenary.reload").reload_module("just")
 end
 
+---comment Splits a string by a given delimiter.
+---@param input string Text to split.
+---@param separator string Delimiter at which the splits are made.
+---@return table Table of split strings.
 function M.splitString(input, separator)
 	local words = {}
 	for word in string.gmatch(input, "([^" .. separator .. "]+)") do
@@ -16,11 +23,21 @@ function M.splitString(input, separator)
 	return words
 end
 
+---comment Opens the quickfix window.
 function M.openQuickfix()
 	vim.api.nvim_command("copen")
 end
 
+---comment Clears the quickfix window.
+function M.clearQuickfix()
+	print("clearing quickfix")
+	vim.fn.setqflist({}, "r")
+end
+
+---comment Appends items to the quickfix list.
+---@param arg any Arguments to add to the quickfix list.
 function M.appendToQuickfix(arg)
+	print("appending to quickfix: " .. arg)
 	vim.fn.setqflist({}, "a", {
 		lines = arg,
 		-- efm = "./%f: line %l: %m",
@@ -28,29 +45,20 @@ function M.appendToQuickfix(arg)
 	})
 end
 
+---Sets the quickfix list title.
+---@param arg any Title of the quickfix window.
+function M.setQuickfixTitle(arg)
+	vim.fn.setqflist({}, "a", {
+		title = arg,
+	})
+end
+
+---comment Removes color codes from a string
+---@param lines string String to sanitize.
 function M.sanitize(lines)
 	for i = 1, #lines do
 		lines[i] = (lines[i]):gsub(string.char(27) .. "[[0-9;]*m]", "")
 	end
-end
-
-function M.completeRecipe(args)
-	-- Match the command line arguments to all the available recipes and sugges
-	-- those that contain the arguments
-
-	local suggestionList = {}
-
-	justSummaryJob:sync()
-	local justRecipes = utils.splitString(justSummaryJob:result()[1], " ")
-
-	for _, recipe in pairs(justRecipes) do
-		if string.find(recipe, args) then
-			table.insert(suggestionList, recipe)
-			utils.printTable(suggestionList)
-		end
-	end
-
-	return suggestionList
 end
 
 return M
