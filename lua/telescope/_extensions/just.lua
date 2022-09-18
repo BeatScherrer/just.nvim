19bb7d40b0f960c861bd2ae89799646ab611b7f1
Beat Scherrer <b.scherrer@mt-robot.com>
2022-09-18 18:12:32 +0200

    renamed export

--- lua/telescope/_extensions/just.lua
+++ lua/telescope/_extensions/just.lua
@@ -1,9 +1,9 @@
 return require("telescope").register_extension({
-	setup = function(ext_config, config)
-		-- access extension config and user config
-	end,
+	-- setup = function(ext_config, config)
+	-- 	-- access extension config and user config
+	-- end,
 	exports = {
 		-- Export plugin stuff here
-		just = require("telescope.recipePicker"),
+		justRecipes = require("telescope.recipePicker"),
 	},
 })
